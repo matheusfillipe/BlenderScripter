79c5ef3aa60722e2ceeba3a880385e7f64a117df
matheusfillipeag <matheusfillipeag@gmail.com>
2019-04-08 04:03:10 -0300

    Initial

--- __init__.py
+++ __init__.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import bpy
-#import .execute
 
 
 from bpy.types import Operator 
@@ -18,6 +17,79 @@ bl_info = {
 location=bpy.types.CONSOLE_HT_header
 locationTextEditor=bpy.types.TEXT_MT_text
 
+_BPY_MAIN_OWN = True
+from console_python import *
+
+def add_scrollback(context, text, text_type):
+    for l in text.split("\n"):
+        bpy.ops.console.scrollback_append(context, text=l.replace("\t", "    "), type=text_type)
+
+
+def execute(context, is_interactive):
+    sc = context['space_data']
+    try:
+        line_object = sc.history[-1]
+    except:
+        return {'CANCELLED'}
+    console, stdout, stderr = get_console(hash(context['region']))
+    if _BPY_MAIN_OWN:
+        main_mod_back = sys.modules["__main__"]
+        sys.modules["__main__"] = console._bpy_main_mod
+    from contextlib import (
+        redirect_stdout,
+        redirect_stderr,
+    )
+    class redirect_stdin(redirect_stdout.__base__):
+        _stream = "stdin"
+    with redirect_stdout(stdout), \
+            redirect_stderr(stderr), \
+            redirect_stdin(None):
+        line = ""  # in case of encoding error
+        is_multiline = False
+        try:
+            line = line_object.body
+            line_exec = line if line.strip() else "\n"
+            is_multiline = console.push(line_exec)
+        except:
+            import traceback
+            stderr.write(traceback.format_exc())
+    if _BPY_MAIN_OWN:
+        sys.modules["__main__"] = main_mod_back
+    stdout.seek(0)
+    stderr.seek(0)
+    output = stdout.read()
+    output_err = stderr.read()
+    sys.last_traceback = None
+    stdout.truncate(0)
+    stderr.truncate(0)
+    if hash(sc) != hash(context['space_data']):
+        return {'FINISHED'}
+
+    #bpy.ops.console.scrollback_append(text=sc.prompt + line, type='INPUT')
+
+    if is_multiline:
+        sc.prompt = PROMPT_MULTI
+        if is_interactive:
+            indent = line[:len(line) - len(line.lstrip())]
+            if line.rstrip().endswith(":"):
+                indent += "    "
+        else:
+            indent = ""
+    else:
+        sc.prompt = PROMPT
+        indent = ""
+
+    # insert a new blank line
+    #bpy.ops.console.history_append(context, text=indent, current_character=0, remove_duplicates=True)
+    sc.history[-1].current_character = len(indent)
+
+    if output:
+        add_scrollback(context, output, 'OUTPUT')
+    if output_err:
+        add_scrollback(context, output_err, 'ERROR')
+
+    return {'FINISHED'}
+
 class BlenderController(bpy.types.Operator):
     """Runs the active script on the Text Editor on the Python Console"""
     bl_idname = "object.blendercontroller_operator"
@@ -29,32 +101,30 @@ class BlenderController(bpy.types.Operator):
         current=None
         override=None
         context = bpy.context
+        script_name = ''
 
         for area in bpy.context.screen.areas:
             if area.type == 'TEXT_EDITOR':
                 space = area.spaces.active
                 current=repr(space.text)
+                script_name = space.text.name
             if area.type == 'CONSOLE':
                 override = bpy.context.copy()
                 override['space_data'] = area.spaces.active
                 override['region'] = area.regions[-1]
                 override['area'] = area
-                bpy.ops.screen.screen_full_area(override, use_hide_panels=True)
-
-
-                print(context.area, override['area'])
-                print(context.screen, override['screen'])
-                print(context.window, override['window'])
                 override['screen'] = context.screen
-                override["area"] = context.area
-               
-
 
         if current is None or override is None:
             self.report(type='ERROR',message="Please make sure you have a Console and Text Editor visible")
+            return {'CANCELLED'}
 
+        bpy.ops.console.clear_line(override)
+        bpy.ops.console.scrollback_append(override, text='Running Script: '+script_name, type='OUTPUT')
         bpy.ops.console.insert(override,text="exec("+current+".as_string())\n")
-        bpy.ops.console.execute(override, interactive=True)
+        execute(override, is_interactive=True)
+        bpy.ops.console.clear_line(override)
+
 
         return {'FINISHED'} 
         
@@ -71,11 +141,12 @@ def register():
     bpy.utils.register_class(BlenderController) 
     location.append(add_object_button)
     locationTextEditor.append(add_object_button)
-      
-    wm = bpy.context.window_manager   
+    
+    wm = bpy.context.window_manager
     km = wm.keyconfigs.addon.keymaps.new(name='Window', region_type='WINDOW', space_type='EMPTY')
 
     kmi = km.keymap_items.new(BlenderController.bl_idname, 'F9', 'PRESS', ctrl=False, shift=False)
+
     addon_keymaps.append((km, kmi))
  
 def unregister():
